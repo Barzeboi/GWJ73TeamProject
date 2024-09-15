e1643cfc7b0e0d3272f7ee965d1fc1e2a068cfeb
Barzeboi <berzenoki@gmail.com>
2024-09-15 08:30:51 -0500

    Piece can now rotate

--- Scripts/tetrominoes.gd
+++ Scripts/tetrominoes.gd
@@ -93,6 +93,8 @@ func _process(delta: float) -> void:
 		steps[1] += 10
 	elif Input.is_action_pressed("Down"):
 		steps[2] += 10
+	elif Input.is_action_just_pressed("Rotate"):
+		_rotate_piece()
 	#apply downward movement every frame
 	steps[2] += speed
 	for f in range(steps.size()):
@@ -126,12 +128,21 @@ func _draw_piece(piece, pos, atlas):
 	for f in piece:
 		active_layer.set_cell(pos + f, tile_id, atlas)
 		
+func _rotate_piece():
+	if _can_rotate():
+		_clear_piece()
+		rotation_index = (rotation_index + 1) % 4
+		active_piece = piece_type[rotation_index]
+		_draw_piece(active_piece, cur_pos, piece_atlas)
+		
+		
 func _move_pieces(dir):
 	if _can_move(dir):
 		_clear_piece()
 		cur_pos += dir
 		_draw_piece(active_piece, cur_pos, piece_atlas)
-
+		
+#The following three functions prevent the piece from moving outside of the boarder
 func _can_move(dir):
 	#check if there is space to move
 	var cm = true
@@ -139,6 +150,14 @@ func _can_move(dir):
 		if not _is_free(f + cur_pos + dir):
 			cm = false
 	return cm 
+
+func _can_rotate():
+	var cr = true
+	var temp_rotation_index = (rotation_index + 1) % 4
+	for f in piece_type[temp_rotation_index]:
+		if not _is_free(f + cur_pos):
+			cr = false
+	return cr
 
 func _is_free(pos):
 	return board_layer.get_cell_source_id(pos) == -1
